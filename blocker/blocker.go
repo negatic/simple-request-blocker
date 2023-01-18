175a2324b28046f26778f263d8ef6cc7eb15e1a6
SiNA <Apple@MacBooks-MacBook-Air-2.local>
2023-01-18 17:43:18 +0330

    run function

--- blocker/blocker.go
+++ blocker/blocker.go
@@ -1,6 +1,9 @@
 package blocker
 
-import "time"
+import (
+	"flag"
+	"time"
+)
 
 type Blocker struct {
 	Port              string
@@ -18,4 +21,13 @@ func NewBlocker(port string, blockEveryRequest bool, urlList []string, startTime
 		StartTime:         startTime,
 		EndTime:           endTime,
 	}
+}
+
+func Run() error {
+	blockEveryRequest := flag.Bool("blockeveryrequest", false, "Set to true to block all requests")
+	blockList := flag.String("blocklist", "", "Sites to block,seprate by a comma. Example: --blocklist google.com,github.com")
+	port := flag.String("port", "1080", "the port to listen on, Example: --port 1080")
+	startTime := flag.String("starttime", "10:00", "The time to start the blocking. Example: --starttime 10:00")
+	endTime := flag.String("endtime", "12:00", "The time to end the blocking. Example: --endtime 12:00")
+	flag.Parse()
 }
